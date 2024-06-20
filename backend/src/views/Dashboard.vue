127d71fd252524b2861160bbb97fec298adfc342
Cherry Mg Mg <cherry@kali.Min>
2024-06-20 12:01:49 -0400

    vue and laravel api

---
+++ backend/src/views/Dashboard.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+
+</script>
+
+
+<template>
+    <h2 class="text-red-800">Dashboard </h2>
+</template> 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1 class="text-green-700 text-7xl mt-5">welcome our api crash center ...</h1>
+  </main>
+</template>
